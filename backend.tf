fbbab1b57c3a5191d0f58488cfb494de4b1ac233
unknown <nyoyo@gmail.com>
2024-03-20 00:21:37 -0400

    vpc-project

---
+++ backend.tf
@@ -0,0 +1,13 @@
+ terraform {
+    backend "s3" {
+    bucket         = "my-stephe-bucket"
+    key            = "week10/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform_State_lock"
+  }
+ }
+
+
+  
+  
